38608eff456358178591ce922f191b3a3ed27226
Silas  Owu <silas.owu@amalitech.com>
2023-02-16 13:39:58 -0800

    Changes done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,12 +6,12 @@ const Modal = ({visible,onClose}) => {
   if (!visible) return null;
 
   return (
-    <div onClick={()=> onClose()} className=' min-h-screen w-full laptop:fixed laptop:flex laptop:flex-row laptop:justify-center laptop:items-center laptop:inset-0 laptop:bg-black laptop:opacity-90 laptop:z-10'>
-        <div  className='  px-[2rem] rounded-[0.5rem] flex flex-col justify-center gap-[4rem] items-start laptop:bg-white laptop:w-[25rem] laptop:mt-[10rem] laptop:h-[25.9375rem] '>
+    <div onClick={()=> onClose()} className='mobile:z-10 min-h-screen  w-full mobile:bg-white  mobile:justify-center mobile:items-center absolute  laptop:flex laptop:flex-row laptop:justify-center laptop:items-center inset-0 laptop:bg-black laptop:opacity-95 laptop:z-10'>
+        <div  className='mobile:flex mobile:flex-col mobile:absolute mobile:inset-0 px-[2rem] rounded-[0.5rem] flex flex-col justify-center gap-[4rem] items-start laptop:bg-white laptop:w-[25rem] laptop:mt-[10rem] laptop:h-[25.9375rem] '>
             <div className='flex flex-row gap-[14rem]'>
-            <h4 className='font-barlow font-bold text-[#3B4262] text-[2rem] leading-[2rem]'>RULES</h4>
+            <h4 className='font-barlow mobile:absolute mobile:left-[39%] mobile:top-[20%] font-bold text-[#3B4262] text-[2rem] leading-[2rem]'>RULES</h4>
             
-            <img src={Close} onClick={onClose} className='cursor-pointer self-center w-[1.193125rem] h-[1.193125rem]'alt=''/>
+            <img  src={Close} onClick={onClose} className='mobile:absolute mobile:top-[88%] mobile:left-[50%] mobile:inset-0 cursor-pointer self-center w-[1.193125rem] h-[1.193125rem]'alt=''/>
             </div>
             <div className=' self-center laptop:w-[19.0625rem] laptop:h-[16.9375rem]'>
               <img src={Rules} alt=""/>
